17dd10d4d50e97faae479caa546317a192e7434c
yuanping.wyp <yuanping.wyp@yuanpingwypdeMacBook-Pro.local>
2022-04-24 18:25:38 +0800

    test

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 import os
 
+
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
